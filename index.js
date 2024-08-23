591837e17df850558ea50fd5b1bda5deecceaa6a
Tesfatsion-Gudeta <tesfatsiongudeta7@gmail.com>
2024-08-23 09:22:56 +0300

    building a restful api practise

---
+++ index.js
@@ -0,0 +1,91 @@
+const Joi=require('joi')
+const express= require('express')
+const app=express()
+
+app.use(express.json())
+
+const courses=[{id:1,name:'course1'},{id:2,name:'course2'},{id:3,name:'course3'}]
+
+app.get('/',(req,res)=>{
+    res.send('hello world!!!')
+})
+
+
+app.get('/api/courses',(req,res)=>{
+    res.send(courses)
+})
+
+app.get('/api/course/:id',(req,res)=>{
+    // res.send(req.params.id)
+    // res.send(req.query)
+
+    const course=courses.find(c=>c.id===parseInt(req.params.id))
+    if(!course) return res.status(404).send('the course with that specific id is not found')
+    res.send(course)
+})
+
+app.post('/api/courses', (req,res)=>{
+
+   const {error}=validateCourse(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+   
+    const course={ id:courses.length+1,
+        name:req.body.name}
+
+    courses.push(course)
+    res.send(course)
+
+})
+
+
+
+//put request: updating courses
+
+app.put('/api/course/:id',(req,res)=>{
+
+    const course=courses.find(c=>c.id===parseInt(req.params.id))
+    if(!course) return res.status(404).send('course not found')
+ 
+    const {error}=validateCourse(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    course.name=req.body
+    res.send(course)
+
+
+})
+
+//delete request:for deleting the courses
+
+app.delete('/api/course/:id',(req,res)=> {
+    const course=courses.find(c=>c.id===parseInt(req.params.id))
+
+    if(!course) return res.status(404).send('course not found')
+
+    const index=courses.indexOf(course)
+    courses.splice(index,1)
+
+    res.send(course)
+
+})
+
+
+//input validation
+function validateCourse(name){
+
+
+    const schema=Joi.object({name:Joi.string().min(3).required()})
+
+    return schema.validate(name)
+
+
+
+    
+}
+
+
+
+
+
+const port=process.env.PORT || 3000
+app.listen(port,()=>{console.log(`listening to port ${port}...`)})
